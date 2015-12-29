c1a6868838251f0d8818b422a07dc721630ce873
yaakov300 <yaacov300@gmail.com>
2015-12-29 14:11:55 +0200

    update main

--- alertsStation/main.py
+++ alertsStation/main.py
@@ -1,4 +1,9 @@
 from models.agencyDB import Agency
+from models.RoutesDB import Routes
+from models.TripsDB import Trips
+from models.StopTimeDB import StopTime
+from models.StopsDB import Stops
+
 import webapp2
 #http://alertsstation-1172.appspot.com/agency?id=2
 class MainHandler(webapp2.RequestHandler):
@@ -23,7 +28,24 @@ class MainHandler(webapp2.RequestHandler):
             if (id==2):#get all DB
                 agency=Agency.getAllAgency()
                 self.post(agency)
-
+            if (id == 3):#read data from file to DB
+                route = Routes.readFromGtfsRoutes()
+                self.post("read from file route")
+            if (id == 4):#read data from file to DB
+                route = Routes.getAllRoutes()
+                self.post(route)
+            if (id == 5):#read data from file to DB
+                trips = Trips.readFromGtfsTrips()
+                self.post("read from file trips")
+            if (id == 6):#read data from file to DB
+                trips = Trips.getAllTrips()
+                self.post(trips)
+            if (id == 7):#read data from file to DB
+                stops = Stops.readFromGtfsStops()
+                self.post("read from file stops")
+            if (id == 8):#read data from file to DB
+                stops = Stops.getAllStops()
+                self.post(stops)
 
     def post(self,response):
         self.response.write(response)
