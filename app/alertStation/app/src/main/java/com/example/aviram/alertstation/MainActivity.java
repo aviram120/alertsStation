517d01ca50067750a34fddd8cf45f11b77919efd
aviram120 <aviram1202@gmail.com>
2016-02-05 18:57:38 +0200

    add serves and task for alert

--- app/alertStation/app/src/main/java/com/example/aviram/alertstation/MainActivity.java
+++ app/alertStation/app/src/main/java/com/example/aviram/alertstation/MainActivity.java
@@ -39,7 +39,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MainActivity extends Activity implements View.OnClickListener,LocationListener {
+public class MainActivity extends Activity implements View.OnClickListener {
     private final String SERVER_URL="http://alertsstation-1172.appspot.com";
     private Spinner spinner_copmany,spinner_city,spinner_line,spinner_station;
     private Button btSave;
@@ -67,31 +67,28 @@ public class MainActivity extends Activity implements View.OnClickListener,Locat
         setContentView(R.layout.activity_main);
 
         activity=this;//save the activity
-
-        //init location
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+       /* //init location
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         mLocationListener = this;
-
+*/
         initialization();
 
         getCompanyFromServer();
 
     }
-
-    public void addItemsOnSpinner(Spinner spinner_id,List<String> list)
-    {
+    public void addItemsOnSpinner(Spinner spinner_id,List<String> list) {
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, list);//TODO change the design
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner_id.setAdapter(dataAdapter);
     }
-
     private void initialization() {
 
-        _userLocation = getLastKnownLocation();
+       /* _userLocation = getLastKnownLocation();
 
         dialogLocation = new ProgressDialog(this);
-        dialogLocation.setMessage("Getting Location...");
+        dialogLocation.setMessage("Getting Location...");*/
 
         dialog = new ProgressDialog(this);
         dialog.setMessage("Loading Date...");
@@ -113,7 +110,6 @@ public class MainActivity extends Activity implements View.OnClickListener,Locat
         _citesList = new ArrayList<CitesData>();
         _stopsList = new ArrayList<StopData>();
     }
-
     public class OnItemSelectedListener implements AdapterView.OnItemSelectedListener {
         public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
 
@@ -137,8 +133,12 @@ public class MainActivity extends Activity implements View.OnClickListener,Locat
         public void onNothingSelected(AdapterView parent) {}
     }
 
-    private void getCompanyFromServer()
-    {
+    // =============================================================
+    // API Request
+    // =============================================================
+    private void getCompanyFromServer() {
+        //the function get from server the company(mane , id)
+
         dialog.show();
         String url=SERVER_URL+"/api?act=1";
         queue = Volley.newRequestQueue(this);
@@ -149,7 +149,7 @@ public class MainActivity extends Activity implements View.OnClickListener,Locat
                     public void onResponse(String response) {
                         JSONArray jsonEventList;
                         try {
-                            dialog.hide();//stop thr dialog
+                            dialog.hide();//stop the dialog
                             response = response.replaceAll("\\r\\n", "");
                             jsonEventList = new JSONArray(response);
                         } catch (JSONException e) {
@@ -169,14 +169,11 @@ public class MainActivity extends Activity implements View.OnClickListener,Locat
                                     tempCompanyData=new CompanyData(jsonCompany,i);
                                     _companyList.add(tempCompanyData);
 
-                                    list_cop.add(jsonCompany.getString("agencyName"));
-
-                                    //Log.i("aviramLog", i + "" + jsonCompany.getString("agencyName"));
+                                    list_cop.add(jsonCompany.getString("agencyName"));//add to list
                                 }
                                 addItemsOnSpinner(spinner_copmany, list_cop);//add to spinner
                             }catch (JSONException e){}
                         }
-
                     }
 
                 },new Response.ErrorListener(){
@@ -191,9 +188,9 @@ public class MainActivity extends Activity implements View.OnClickListener,Locat
         queue.add(request);
 
     }
+    private void getCitesFromServer() {
+        //the function get all cites from server by company
 
-    private void getCitesFromServer()
-    {
        dialog.show();
        String url = SERVER_URL+"/api?act=2&agency_id=" + _companyList.get((int) spinner_copmany.getSelectedItemId()).getCompany_id();
        queue = Volley.newRequestQueue(this);
@@ -242,11 +239,9 @@ public class MainActivity extends Activity implements View.OnClickListener,Locat
 
        queue.add(request);
     }
+    private void getRoutesFromServer() {
+        //the function get all Routes from server by agency_id and city_id
 
-    private void getRoutesFromServer()
-    {
-        //long cityLong = toAscii(spinner_city.getSelectedItem().toString());
-        //Log.i("aviramLog","cityLong = " + cityLong);
         dialog.show();
         String url=SERVER_URL+"/api?act=3&agency_id="+ _companyList.get((int) spinner_copmany.getSelectedItemId()).getCompany_id()+
                 "&city_id="+_citesList.get((int) spinner_city.getSelectedItemId()).getCity_id();
@@ -299,9 +294,9 @@ public class MainActivity extends Activity implements View.OnClickListener,Locat
         queue.add(request);
 
     }
+    private void getStopsFromServer() {
+        //the function get all Stops station from server by route_id
 
-    private void getStopsFromServer()
-    {
         dialog.show();
         String url=SERVER_URL+"/api?act=4&route_id="+_routesList.get((int) spinner_line.getSelectedItemId()).getRoute_id();
 
@@ -358,141 +353,17 @@ public class MainActivity extends Activity implements View.OnClickListener,Locat
 
         if (v.getId() == R.id.btSave)//right text view
         {
-            /*Log.i("aviramLog","spinner1:getSelectedItem: " +spinner_copmany.getSelectedItem().toString());
-            Log.i("aviramLog","spinner1:getSelectedItemId: "+spinner_copmany.getSelectedItemId());
-            Log.i("aviramLog", "company name "+_companyList.get((int) spinner_copmany.getSelectedItemId()).getCompany_name());
-            Log.i("aviramLog", "id "+_companyList.get((int) spinner_copmany.getSelectedItemId()).getCompany_id());*/
-
-
-           /* Log.i("aviramLog", "city_name " + spinner_city.getSelectedItem());
-            getRoutesFromServer();*/
-
-            /*Log.i("aviramLog", "id "+_companyList.get((int) spinner_copmany.getSelectedItemId()).getCompany_id());
-            String str = _companyList.get((int) spinner_copmany.getSelectedItemId()).getCompany_name().replaceAll("\\r\\n", "");
-            Log.i("aviramLog", "company name: "+str);*/
-
-            Log.i("aviramLog","getStop_name:"+_stopsList.get((int) spinner_station.getSelectedItemId()).getStop_name());
+           /* Log.i("aviramLog","getStop_name:"+_stopsList.get((int) spinner_station.getSelectedItemId()).getStop_name());
             Log.i("aviramLog", "getStop_lat:" + _stopsList.get((int) spinner_station.getSelectedItemId()).getStop_lat());
             Log.i("aviramLog","getStop_lon:"+_stopsList.get((int) spinner_station.getSelectedItemId()).getStop_lon());
             Log.i("aviramLog", "getStop_sequence:" + _stopsList.get((int) spinner_station.getSelectedItemId()).getStop_sequence());
+*/
+            AlarmReceiver alarm=new AlarmReceiver();
+            alarm.setAlarm(activity,locationManager,_stopsList.get((int) spinner_station.getSelectedItemId()));
 
-            getLocationFromSystem();
 
         }
     }
-
-
-
-    // =============================================================
-    // GPS
-    // =============================================================
-    private void getLocationFromSystem() {
-        // the function get the location from GPS and send it to server
-
-        boolean isGPSAvailable = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-        boolean isWIFIAvailable = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-        Log.i("aviramLog", "in my location gps"+isGPSAvailable);
-        Log.i("aviramLog", "in my location wifi" + isWIFIAvailable);
-        if (isGPSAvailable) {
-            //get run time permission
-            permissionManager = new PermissionManager(activity, new PermissionManager.OnPermissionListener() {
-                public void OnPermissionChanged(boolean permissionGranted) {
-                    Log.d("aviramLog", "permissionGranted: " + permissionGranted);
-                    if (permissionGranted) {
-                        try{//get location -updates
-                            dialogLocation.show();
-                            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, SECOND * 10, MIN_DISTANCE, mLocationListener);
-                            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, SECOND * 10, MIN_DISTANCE, mLocationListener);
-
-                        } catch (SecurityException e) {   }
-                    }
-                }
-            });
-        }
-        else//if the location not available
-        {
-            showSettingsAlert();
-        }
-    }
-    private void showSettingsAlert() {
-        //the function show a alert dialog-of setting
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(activity);
-
-        // Setting Dialog Title
-        alertDialog.setTitle("GPS is settings");
-
-        // Setting Dialog Message
-        alertDialog.setMessage("GPS is not enabled. Do you want to go to settings menu?");
-
-        // On pressing Settings button
-        alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                activity.startActivity(intent);
-            }
-        });
-
-        // on pressing cancel button
-        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
-        });
-
-        // Showing Alert Message
-        alertDialog.show();
-    }
-    public void onLocationChanged(Location location) {
-        //the function get the location and send the data to server to get the weather forecast
-
-        _userLocation=location;
-
-        dialogLocation.hide();
-        Log.i("aviramLog", "my Location " + location.toString());
-        Log.i("aviramLog", "lat:" + _userLocation.getLatitude());
-        Log.i("aviramLog", "long:" + _userLocation.getLongitude());
-        Log.i("aviramLog", "acc:" + _userLocation.getAccuracy());
-
-    }
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        permissionManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
-    public void onProviderEnabled(String provider) {
-        //provider enabled by user
-    }
-    public void onProviderDisabled(String provider) {
-        //provider disabled by user
-    }
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-        //provider status changed
-    }
-    private Location getLastKnownLocation()
-    {
-
-        try
-        {
-            return locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-        }
-        catch (SecurityException e)
-        {
-            printToast(this, "Can't get last known location");
-        }
-
-        return null;
-    }
-
-
-    // ================================================================
-    // Print Toast
-    // ================================================================
-    public static void printToast(Context context, String str)
-    {
-        Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
-    }
-
-
-
 
 
 }//end
